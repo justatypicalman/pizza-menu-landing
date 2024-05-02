422a1d70240ba030171ada82667d2fbf91a01130
Eric Dango <joegarcia11.jg@gmail.com>
2024-05-02 21:31:06 +0800

    added some conditions

--- src/index.js
+++ src/index.js
@@ -109,12 +109,12 @@ function Menu() {
 function Pizza({ pizzaObj }) {
   // if (pizzaObj.soldOut) return null;
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredient}</p>
-        <span>${pizzaObj.price + 3}</span>
+        <span>{pizzaObj.soldOut ? "Sold out" : `$${pizzaObj.price}` + 3}</span>
       </div>
     </li>
   );
